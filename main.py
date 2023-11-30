3f4b25cbca8be8164cff7f87348e21ee594545f9
LaBerryO <151982297+LaBerryO@users.noreply.github.com>
2023-12-01 07:15:27 +0900

    Verison 1.0
    게임 개발 완료

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ screen_center = [WIDTH/2, HEIGHT/2]
 # 색깔 정의
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+BLACK = (0,0,0)
 
 # 변수 정의
-dirtwidth = WIDTH
-dirtheight = HEIGHT/4
-raindrops = []
+down = []
+up = []
+left = []
+right = []
 last_raindrop_time = pygame.time.get_ticks()
 score = 0
 
@@ -32,30 +34,42 @@ def imgDefine(path, x, y):
     img = pygame.transform.scale(img, (x,y))
     return img
 
-# 이미지 정의
-sky = imgDefine("image/haneul.png",WIDTH,HEIGHT)
-dirt = imgDefine("image/ddang.png",dirtwidth,dirtheight)
-startimg = pygame.image.load("image/gamestart_01.png")
-clickstartimg = pygame.image.load("image/gamestart_02.png")
-waterimg = imgDefine("image/water.png", 100, 100)
-quitimg = pygame.image.load("image/gamequit_01.png")
-clickquitimg = pygame.image.load("image/gamequit_02.png")
-menuimg = imgDefine("image/menu.png", WIDTH,HEIGHT)
+def play_sound(sound):
+    channel = pygame.mixer.find_channel()  # 빈 오디오 채널 찾기
+    if channel:
+        channel.play(sound)
 
-move1 = imgDefine("image/player_0.png", 128, 128)
-move2 = imgDefine("image/player_1.png", 128, 128)
-move3 = imgDefine("image/player_2.png", 128, 128)
-move4 = imgDefine("image/player_3.png", 128, 128)
+# 이미지 정의
+backgroundimg = imgDefine("resource/ui/background.png",WIDTH,HEIGHT).convert()
+startimg = pygame.image.load("resource/ui/gamestart_01.png")
+clickstartimg = pygame.image.load("resource/ui/gamestart_02.png")
+waterimg = imgDefine("resource/object/water.png", 100, 100)
+quitimg = pygame.image.load("resource/ui/gamequit_01.png")
+clickquitimg = pygame.image.load("resource/ui/gamequit_02.png")
+menuimg = imgDefine("resource/ui/menu.png", WIDTH,HEIGHT).convert()
+
+move1 = imgDefine("resource/playermovement/player_0.png", 100, 100)
+move2 = imgDefine("resource/playermovement/player_1.png", 100, 100)
+move3 = imgDefine("resource/playermovement/player_2.png", 100, 100)
+move4 = imgDefine("resource/playermovement/player_3.png", 100, 100)
 
 # 기타 정의
-font = pygame.font.SysFont(None, 50)
+score_font = pygame.font.Font("resource/fonts/jua.ttf",50)
+score_text_position = (10, 10)
+menu_font = pygame.font.Font("resource/fonts/jua.ttf",130)
+
+# 사운드 이펙트 정의
+water_sound1 = pygame.mixer.Sound("resource/sounds/pew1.mp3")
+water_sound2 = pygame.mixer.Sound("resource/sounds/pew2.mp3")
+pygame.mixer.music.load("resource/sounds/background_music.mp3")
 
 
-class Raindrop:
+class bulletDown:
     def __init__(self):
         self.x = rd.randint(100,WIDTH-100)
         self.y = -100
-        self.speed = rd.randint(5,15)
+        self.speed = 10
+        play_sound(water_sound1)
 
     def move(self):
         self.y += self.speed
@@ -64,14 +78,61 @@ class Raindrop:
         screen.blit(waterimg, (self.x, self.y))
 
     def off_screen(self):
-        return self.y > 900
+        return self.y > HEIGHT
+
+class bulletUp:
+    def __init__(self):
+        self.x = rd.randint(100,WIDTH-100)
+        self.y = HEIGHT+100
+        self.speed = 10
+
+    def move(self):
+        self.y -= self.speed
+    
+    def draw(self):
+        screen.blit(waterimg, (self.x, self.y))
+
+    def off_screen(self):
+        return self.y < -100
+
+class bulletRight:
+    def __init__(self):
+        self.x = WIDTH + 100
+        self.y = rd.randint(100,HEIGHT-100)
+        self.speed = 10
+
+    def move(self):
+        self.x -= self.speed
+    
+    def draw(self):
+        screen.blit(waterimg, (self.x, self.y))
+
+    def off_screen(self):
+        return self.x < -100
+
+class bulletLeft:
+    def __init__(self):
+        self.x = -100
+        self.y = rd.randint(100,HEIGHT-100)
+        self.speed = 10
+
+    def move(self):
+        self.x += self.speed
+    
+    def draw(self):
+        screen.blit(waterimg, (self.x, self.y))
+
+    def off_screen(self):
+        return self.x > WIDTH
 
 
 class Player:
     def __init__(self):
         self.x = screen_center[0]
         self.y = screen_center[1]
-        self.speed = 5
+        self.width = 100
+        self.height = 100
+        self.speed = 8
         self.heading = 1
 
     def left(self):
@@ -99,6 +160,15 @@ class Player:
             screen.blit(move3,(self.x,self.y))
         if self.heading == 4:
             screen.blit(move4,(self.x,self.y))
+    
+    def hit_by(self, water):
+        # 플레이어의 충돌 영역 정의
+        player_rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        # water의 충돌 영역 정의
+        water_rect = pygame.Rect(water.x, water.y, 100, 100)
+        # 충돌 체크
+        return player_rect.colliderect(water_rect)
+
 
 class Button:
     def __init__(self, img_in, x, y, width, height, img_act, action = None):
@@ -113,39 +183,46 @@ class Button:
             screen.blit(img_in,(x,y))
 
 def quitGame():
+    pygame.mixer.music.stop()
     pygame.quit()
     sys.exit()
 
 clock = pygame.time.Clock()
 
 def mainmenu():
+    global menu
     menu = True
     while menu:
         clock.tick(75)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quitGame()
-        screen.blit(menuimg, (0,0))
+        screen.fill(WHITE)
+        menu_text = menu_font.render("벌레 전사는 물이 무서워요", True, BLACK)
+        screen.blit(menu_text,(screen_center[0]-600,100))
         startButton = Button(startimg,screen_center[0]-150,screen_center[1]-110,300,200,clickstartimg,mainGame)
         quitButton = Button(quitimg,screen_center[0]-150,screen_center[1]+100,300,200,clickquitimg,quitGame)
 
         pygame.display.flip()
 
 def mainGame():
-    global move, last_raindrop_time, score
+    global move, last_raindrop_time, score, menu
+    pygame.mixer.music.play(-1)
+    timer = 0
+    whattime = 0
+    menu = False
     players = Player()
     running = True
     while running:
-        clock.tick(75)
-        screen.blit(sky,(0,0))
-        screen.blit(dirt,(0,HEIGHT-dirtheight))
+        clock.tick(60)
+        screen.blit(backgroundimg,(0,0))
 
         # 이벤트 처리
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 quitGame()
-        
+
         pressed_keys = pygame.key.get_pressed()
 
         players.draw()
@@ -163,17 +240,81 @@ def mainGame():
         
         current_timef = pygame.time.get_ticks()
         if current_timef - last_raindrop_time > 3000:  # 3초에 한번
-            raindrops.append(Raindrop())
+            down.append(bulletDown())
+            up.append(bulletUp())
+            right.append(bulletRight())
+            left.append(bulletLeft())
             last_raindrop_time = current_timef
 
-        i = 0
-        while i < len(raindrops) :    
-            raindrops[i].move()
-            raindrops[i].draw()
-            if raindrops[i].off_screen():
-                del raindrops[i]
-                i -= 1
-            i += 1  
+        a=0
+        b=0
+        c=0
+        d=0
+        
+        while a < len(down):
+            down[a].move()
+            down[a].draw()
+            if players.hit_by(down[a]):
+                running = False
+                gameOver()
+            elif down[a].off_screen():
+                del down[a]
+                a -= 1
+            a += 1
+        while b < len(left):
+            left[b].move()
+            left[b].draw()
+            if players.hit_by(left[b]):
+                running = False
+                gameOver()
+            elif left[b].off_screen():
+                del left[b]
+                b -= 1
+            b += 1
+        while c < len(up):
+            up[c].move()    
+            up[c].draw()
+            if players.hit_by(up[c]):
+                running = False
+                gameOver()
+            elif up[c].off_screen():
+                del up[c]
+                c -= 1
+            c += 1
+        while d < len(right):
+            right[d].move()
+            right[d].draw()
+            if players.hit_by(right[d]):
+                running = False
+                gameOver()
+            elif right[d].off_screen():
+                del right[d]
+                d -= 1
+            d += 1
+        
+        timer = pygame.time.get_ticks()
+        if timer - whattime > 1000:
+            score += 1
+            whattime = timer
+
+        score_text = score_font.render(f"Score: {score}", True, RED)
+        screen.blit(score_text, score_text_position)
+        pygame.display.flip()
+
+def gameOver():
+    restart = True
+    pygame.mixer.music.stop()
+    while restart:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitGame()
+        screen.fill(WHITE)
+        game_over_text = score_font.render("Game Over", True, BLACK)
+        score_text = score_font.render(f"Your Score: {score}", True, BLACK)
+
+        screen.blit(game_over_text, (screen_center[0] - 200, screen_center[1] - 50))
+        screen.blit(score_text, (screen_center[0] - 150, screen_center[1] + 50))
 
         pygame.display.flip()
     
